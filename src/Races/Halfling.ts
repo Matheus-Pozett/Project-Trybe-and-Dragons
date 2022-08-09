e2f2530156e80a81bf6a9bb68f5797c0146c57d8
Matheus Pozett <maa_souza11@hotmail.com>
2022-08-09 18:08:36 -0300

    Requisito 2: criando classes que herdam de Race

---
+++ src/Races/Halfling.ts
@@ -0,0 +1,20 @@
+import Race from './Race';
+
+export default class Halfling extends Race {
+  private _maxLifePoints: number;
+  private static _HALFLINGINSTANCE = 0;
+
+  constructor(name: string, dexterity: number) {
+    super(name, dexterity);
+    this._maxLifePoints = 60;
+  }
+
+  public get maxLifePoints(): number {
+    return this._maxLifePoints;
+  }
+
+  public static createdRacesInstances(): number {
+    this._HALFLINGINSTANCE += 1;
+    return this._HALFLINGINSTANCE; 
+  }
+}
